451e10913483fecc9ad11552a442f6898427e6a2
Charles <charles.bernard15@hotmail.com>
2020-09-22 15:13:10 +0200

    Intégration template

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -1,9 +1,34 @@
- @extends('template.index')
+{{--
+  ./resources/views/pages/show.blade.php
+  Variables disponibles :
+    $page OBJ(id, titre, sousTitre, texte, titreMenu, image, created_at, ...)
+--}}
+@extends('template.index')
 
- @section('content')
-    <h1>{{ $page->titre }}</h1>
-    <div class="">
-      {{ $page->texte }}
+@section('content')
+  <!-- Page Header -->
+  <header class="masthead" style="background-image: url({{ asset('assets/img/' . $page->image) }})">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="site-heading">
+            <h1>{{ $page->titre }}</h1>
+            <span class="subheading">{{ $page->sousTitre }}</span>
+          </div>
+        </div>
+      </div>
     </div>
+  </header>
 
-  @endsection
+  <!-- Textes -->
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 col-md-10 mx-auto">
+        <div class="clearfix">
+          {!! html_entity_decode($page->texte) !!}
+        </div>
+        {{-- Ici viennent les contenus complémentaires (posts, formulaire, ...) --}}
+      </div>
+    </div>
+  </div>
+@endsection
